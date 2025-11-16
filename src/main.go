2a45ec673290595cb4e8ec474709ba1218692fbf
Kyohei Harada <54210063+nimowagukari@users.noreply.github.com>
2025-11-16 16:09:12 +0900

    ロギングミドルウェアを追加 (#3)
    * ロギングミドルウェアを追加
    
    * エンドポイントとログ出力内容を調整

--- src/main.go
+++ src/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"time"
 )
 
 type helloWorldHandler struct{}
@@ -12,7 +14,37 @@ func (h *helloWorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello,World.")
 }
 
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(statusCode int) {
+	lrw.statusCode = statusCode
+	lrw.ResponseWriter.WriteHeader(statusCode)
+}
+
+// TODO: どこかのタイミングでテストコードを追加する
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		lrw := &loggingResponseWriter{ResponseWriter: w, statusCode: http.StatusNotFound}
+
+		next.ServeHTTP(lrw, r)
+
+		elaspedTime := time.Since(start)
+		slog.Info("access_log",
+			"remoteAddr", r.RemoteAddr,
+			"url", r.URL,
+			"method", r.Method,
+			"statusCode", lrw.statusCode,
+			"elaspedSeconds", elaspedTime.Seconds())
+	})
+}
+
 func main() {
-	http.Handle("/", &helloWorldHandler{})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	mux := http.NewServeMux()
+	mux.Handle("/hello", loggingMiddleware(&helloWorldHandler{}))
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
